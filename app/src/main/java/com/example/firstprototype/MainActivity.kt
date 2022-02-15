ba8c69d842bc18c43ef2f853d90b0de2b4681431
777leed <khalidkoukou70@gmail.com>
2022-02-15 17:35:09 +0100

    Signup/LogIn by Leed

--- app/src/main/java/com/example/firstprototype/MainActivity.kt
+++ app/src/main/java/com/example/firstprototype/MainActivity.kt
@@ -2,19 +2,95 @@ package com.example.firstprototype
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        auth = Firebase.auth
+
         val textView = findViewById<TextView>(R.id.create_acc)
+        val btn_login = findViewById<Button>(R.id.btn_signin)
+
         textView.setOnClickListener{
-            startActivity(Intent ( this@MainActivity,home::class.java))
+            startActivity(Intent ( this@MainActivity,signup::class.java))
+        }
+        btn_login.setOnClickListener {
+            doLogin()
         }
 
 
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        updateUI(currentUser)
+    }
+
+    private fun doLogin() {
+        val username_s = findViewById<EditText>(R.id.username_s)
+        val password_s = findViewById<EditText>(R.id.password_s)
+        if (username_s.text.toString().isEmpty()) {
+            username_s.error = "Please enter username_s"
+            username_s.requestFocus()
+            return
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(username_s.text.toString()).matches()) {
+            username_s.error = "Please enter valid username_s"
+            username_s.requestFocus()
+            return
+        }
+
+        if (password_s.text.toString().isEmpty()) {
+            password_s.error = "Please enter password"
+            password_s.requestFocus()
+            return
+        }
+
+        auth.signInWithEmailAndPassword(username_s.text.toString(),password_s.text.toString())
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+
+                    updateUI(null)
+                }
+            }
+    }
+
+    private fun updateUI(currentUser: FirebaseUser?) {
+
+        if (currentUser != null) {
+            if(currentUser.isEmailVerified) {
+                startActivity(Intent(this, home::class.java))
+                finish()
+            }else{
+                Toast.makeText(
+                    baseContext, "Please verify your email address.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        } else {
+            Toast.makeText(
+                baseContext, "Login failed.",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
     }
 }
--- app/src/main/java/com/example/firstprototype/signup.kt
+++ app/src/main/java/com/example/firstprototype/signup.kt
@@ -3,12 +3,63 @@ package com.example.firstprototype
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageView
+import android.util.Patterns
+import android.widget.*
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.auth.FirebaseAuth
 
 class signup : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+        auth = FirebaseAuth.getInstance()
+        val btn_signup = findViewById<Button>(R.id.button_started)
+
+        btn_signup.setOnClickListener {
+            signUpUser()
+        }
+
+    }
+    private fun signUpUser() {
+        val email = findViewById<EditText>(R.id.email)
+        val password = findViewById<EditText>(R.id.password)
+        if (email.text.toString().isEmpty()) {
+            email.error = "Please enter email"
+            email.requestFocus()
+            return
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email.text.toString()).matches()) {
+            email.error = "Please enter valid email"
+            email.requestFocus()
+            return
+        }
+
+        if (password.text.toString().isEmpty()) {
+            password.error = "Please enter password"
+            password.requestFocus()
+            return
+        }
+
+        auth.createUserWithEmailAndPassword(email.text.toString(), password.text.toString())
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val user = auth.currentUser
+                    user?.sendEmailVerification()
+                        ?.addOnCompleteListener { task ->
+                            if (task.isSuccessful) {
+                                startActivity(Intent(this, MainActivity::class.java))
+                                finish()
+                            }
+                        }
+                } else {
+                    Toast.makeText(
+                        baseContext, "Sign Up failed. Try again after some time.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
 
     }
 }
