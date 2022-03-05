425efd9f89fd623205ff2bbabbde56b4cb720574
leed lust <khalidkoukou70@gmail.com>
2022-03-05 12:52:12 +0100

    logout bug fixed

--- app/src/main/java/com/example/firstprototype/profil.kt
+++ app/src/main/java/com/example/firstprototype/profil.kt
@@ -20,17 +20,30 @@ class profil : AppCompatActivity() {
         setContentView(R.layout.activity_profil)
         val btn_back = findViewById<ImageView>(R.id.back_arrow)
         val logout = findViewById<TextView>(R.id.logout)
+        val e= findViewById<TextView>(R.id.tv_email)
+        val n= findViewById<TextView>(R.id.tv_profile_fragment_username)
         btn_back.setOnClickListener {
             startActivity(Intent(this@profil, home::class.java))
         }
     logout.setOnClickListener{
         SignOut()
         }
-
+        val user = Firebase.auth.currentUser
+        user?.let {
+            // Name, email address, and profile photo Url
+            val name = user.displayName
+            val email = user.email
+            val photoUrl = user.photoUrl
+            e.text = email
+            n.text=name
+            val emailVerified = user.isEmailVerified
+            val uid = user.uid
+        }
     }
     private fun SignOut(){
         Firebase.auth.signOut()
         startActivity(Intent(this, MainActivity::class.java))
+        finishAffinity()
     }
 
 
